89a60ce488108af8f83d57d6494a5b3d63f5281e
elieldiniz <elielfantichelle@outlook.com.br>
2025-03-28 00:51:59 -0300

    fetch: adicinando curriculo

--- frontend/src/app/(paginas)/page.tsx
+++ frontend/src/app/(paginas)/page.tsx
@@ -1,9 +1,14 @@
+import Curriculo from "@/components/curriculo";
 import Principal from "@/components/landing/Principal";
+import { obterTecnologias } from "@/function/tecnologias";
 
-export default function Home() {
+export default async function Home() {
+
+  const tecnologia = await obterTecnologias()
   return (
       <div> 
         <Principal/>
+        <Curriculo tecnologias={tecnologia.todas}/>
       </div>
   );
 }
