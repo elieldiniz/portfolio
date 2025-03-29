7cb7687e8caca91fb189800d1e4efdf06fa79d2c
elieldiniz <elielfantichelle@outlook.com.br>
2025-03-28 23:27:09 -0300

    ftech:buscar tecnologias

---
+++ frontend/src/components/tecnologias/Tecnologias.tsx
@@ -0,0 +1,32 @@
+import { Tecnologia } from "@core"
+import Image from "next/image"
+
+export interface TecnologiasProps {
+	lista: Tecnologia[]
+	tamanhoMenor?: boolean
+}
+
+export default function Tecnologias(props: TecnologiasProps) {
+	return props.lista ? (
+		<div className="flex justify-center gap-4 flex-wrap w-4/5">
+			{props.lista.map((tecnologia) => (
+				<div key={tecnologia.id} className="flex flex-col items-center gap-1">
+					<span
+						className={`
+							relative h-9 w-9 rounded-xl overflow-hidden
+							${!props.tamanhoMenor && "sm:h-12 sm:w-12"}
+						`}
+					>
+						<Image
+							src={tecnologia.imagem}
+							alt={tecnologia.nome}
+							fill
+							className="object-contain"
+						/>
+					</span>
+					<span className="text-[10px] text-zinc-400">{tecnologia.nome}</span>
+				</div>
+			))}
+		</div>
+	) : null
+}
