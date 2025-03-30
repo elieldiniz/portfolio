d6c0d1e508d32833aa4af14bad2b6012f4149ff8
elieldiniz <elielfantichelle@outlook.com.br>
2025-03-29 23:46:08 -0300

    fetch: atualização

--- frontend/src/app/(paginas)/page.tsx
+++ frontend/src/app/(paginas)/page.tsx
@@ -1,14 +1,24 @@
-import Curriculo from "@/components/curriculo";
-import Principal from "@/components/landing/Principal";
-import { obterTecnologias } from "@/function/tecnologias";
+import { obterProjetos } from "@/function/projetos"
+import { obterTecnologias } from "@/function/tecnologias"
+import Container from "@/components/shared/Container"
+import Curriculo from "@/components/curriculo"
+import Principal from "@/components/landing/Principal"
+import Projetos from "@/components/projetos/Projeto"
 
 export default async function Home() {
+	const tecnologias = await obterTecnologias()
+	const projetos = await obterProjetos()
 
-  const tecnologia = await obterTecnologias()
-  return (
-      <div> 
-        <Principal tecnologias={tecnologia.destaques}/>
-        <Curriculo tecnologias={tecnologia.todas}/>
-      </div>
-  );
+	return (
+		<div>
+			<Principal tecnologias={tecnologias.destaques} />
+			<Container className="py-16 flex flex-col items-center gap-10">
+				<Curriculo tecnologias={tecnologias.todas} />
+				<Projetos titulo="Destaque" lista={projetos.destaques} />
+				<Projetos titulo="Web" lista={projetos.web} />
+				<Projetos titulo="Mobile" lista={projetos.mobile} />
+				<Projetos titulo="Jogos" lista={projetos.jogos} />
+			</Container>
+		</div>
+	)
 }
